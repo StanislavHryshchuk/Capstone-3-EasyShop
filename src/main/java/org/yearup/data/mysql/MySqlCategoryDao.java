c518536e5577ec7e2e47ac5013a4b9956f606a3e
Stanislav Hryschuk <stas.hryshchuk@gmail.com>
2025-06-20 13:41:58 -0700

    finished delete Method

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -95,13 +95,23 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
         }catch (SQLException e){
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void delete(int categoryId)
     {
-        // delete category
+        String query = "DELETE FROM category \n" +
+                "WHERE category_id = ?;";
+        try(
+                Connection connection = getConnection();
+                PreparedStatement ps = connection.prepareStatement(query);
+        ) {
+            ps.setInt(1,categoryId);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private Category mapRow(ResultSet row) throws SQLException
